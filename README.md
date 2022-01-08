04b5e34ad2ba3fa0f69eb1d695659844a4267b25
Katrina Verey <kn.verey@gmail.com>
2022-01-08 18:43:29 -0500

    Initial landing page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# kats.cloud
+# Kat's Cloud
+
+This repo hosts the code for https://kats.cloud.
+
+![](https://cdn.shopify.com/s/files/1/0593/0005/3048/files/cumulus-cat_50x@2x.png?v=1641439823)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "cc-gold": "#D0AF76",
+        "cc-brown": "#53515A",
+      }
+    },
+  },
+  plugins: [],
+}
